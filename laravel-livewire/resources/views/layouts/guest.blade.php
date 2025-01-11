96816a7533ee28fd61962c4a091ff1d4a81f6cce
Alexis Tan <alexis@sen-dev.com>
2025-01-11 16:29:34 +0100

    Move theme switcher to a reusable component
    The themes have also been split up into light and dark mode, and then sorted in alphabetic order.

--- laravel-livewire/resources/views/layouts/guest.blade.php
+++ laravel-livewire/resources/views/layouts/guest.blade.php
@@ -20,40 +20,7 @@
             <p class="-mt-1.5 ml-6 font-semibold text-lg text-secondary">{{ config('app.descriptor') }}</p>
         </div>
 
-        <select class="max-w-xs select-bordered select" data-choose-theme>
-            <option disabled selected>Pick your theme</option>
-            <option value="light">Default</option>
-            <option value="cupcake">Cupcake</option>
-            <option value="bumblebee">Bumblebee</option>
-            <option value="emerald">Emerald</option>
-            <option value="corporate">Corporate</option>
-            <option value="synthwave">Synthwave</option>
-            <option value="retro">Retro</option>
-            <option value="cyberpunk">Cyberpunk</option>
-            <option value="valentine">Valentine</option>
-            <option value="halloween">Halloween</option>
-            <option value="garden">Garden</option>
-            <option value="forest">Forest</option>
-            <option value="aqua">Aqua</option>
-            <option value="lofi">Lofi</option>
-            <option value="pastel">Pastel</option>
-            <option value="fantasy">Fantasy</option>
-            <option value="wireframe">Wireframe</option>
-            <option value="black">Black</option>
-            <option value="luxury">Luxury</option>
-            <option value="dracula">Dracula</option>
-            <option value="cmyk">Cmyk</option>
-            <option value="autumn">Autumn</option>
-            <option value="business">Business</option>
-            <option value="acid">Acid</option>
-            <option value="lemonade">Lemonade</option>
-            <option value="night">Night</option>
-            <option value="coffee">Coffee</option>
-            <option value="winter">Winter</option>
-            <option value="dim">Dim</option>
-            <option value="nord">Nord</option>
-            <option value="sunset">Sunset</option>
-        </select>
+        <x-theme-selector />
     </header>
 
     <main class="bg-base-200 min-h-screen hero">
--- laravel-livewire/resources/views/layouts/header.blade.php
+++ laravel-livewire/resources/views/layouts/header.blade.php
@@ -8,40 +8,7 @@
         </div>
 
         <div class="flex items-center gap-4 navbar-end">
-            <select name="choose-theme" class="sm:block hidden max-w-xs select-bordered select" data-choose-theme>
-                <option disabled selected>Pick your theme</option>
-                <option value="light">Default</option>
-                <option value="cupcake">Cupcake</option>
-                <option value="bumblebee">Bumblebee</option>
-                <option value="emerald">Emerald</option>
-                <option value="corporate">Corporate</option>
-                <option value="synthwave">Synthwave</option>
-                <option value="retro">Retro</option>
-                <option value="cyberpunk">Cyberpunk</option>
-                <option value="valentine">Valentine</option>
-                <option value="halloween">Halloween</option>
-                <option value="garden">Garden</option>
-                <option value="forest">Forest</option>
-                <option value="aqua">Aqua</option>
-                <option value="lofi">Lofi</option>
-                <option value="pastel">Pastel</option>
-                <option value="fantasy">Fantasy</option>
-                <option value="wireframe">Wireframe</option>
-                <option value="black">Black</option>
-                <option value="luxury">Luxury</option>
-                <option value="dracula">Dracula</option>
-                <option value="cmyk">Cmyk</option>
-                <option value="autumn">Autumn</option>
-                <option value="business">Business</option>
-                <option value="acid">Acid</option>
-                <option value="lemonade">Lemonade</option>
-                <option value="night">Night</option>
-                <option value="coffee">Coffee</option>
-                <option value="winter">Winter</option>
-                <option value="dim">Dim</option>
-                <option value="nord">Nord</option>
-                <option value="sunset">Sunset</option>
-            </select>
+            <x-theme-selector />
 
             <div class="dropdown dropdown-end">
                 <div tabindex="0" role="button" class="bg-base-100 m-1 btn">
