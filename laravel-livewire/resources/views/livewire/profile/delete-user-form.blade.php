f2d9fdf6f7d71b71d7c64c784e678e9b7d5b42e7
Alexis Tan <alexis@sen-dev.com>
2025-01-15 13:10:05 +0100

    Move modal dialogs into a reusable component

--- laravel-livewire/resources/views/livewire/profile/delete-user-form.blade.php
+++ laravel-livewire/resources/views/livewire/profile/delete-user-form.blade.php
@@ -42,59 +42,51 @@ new class extends Component {
     </div>
 
     @teleport('body')
-        <dialog id="deleteAccountModal" class="backdrop-blur-sm backdrop-grayscale modal"
-                x-bind:open="$wire.showModal"
-                x-on:close.stop="$wire.set('showModal', false)"
-                x-on:keydown.escape.window="$wire.set('showModal', false)"
-                x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
-                x-on:keydown.shift.tab.prevent="prevFocusable().focus()">
-            <div class="p-0 modal-box">
-                <div class="flex items-center gap-4 p-6">
-                    <svg class="w-8 h-8 text-primary" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
-                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18">
-                        </path>
-                    </svg>
-                    <h2 class="text-lg font-medium">
-                        {{ __('Are you sure you want to delete your account?') }}
-                    </h2>
-                </div>
-
-                <form wire:submit.prevent="deleteUser" class="pb-6">
-                    <p class="px-6 mt-1 text-sm">
-                        {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
-                    </p>
-
-                    <label class="px-6 mt-6 form-control w-full">
-                        <div class="label">
-                            <span class="label-text">Password</span>
-                        </div>
-
-                        <input wire:model="password"
-                               id="password"
-                               name="password"
-                               type="password"
-                               class="input-bordered w-full max-w-xs input"
-                               placeholder="{{ __('Password') }}" />
-
-                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                    </label>
-
-                    <div class="divider"></div>
-
-                    <div class="px-6 flex justify-end">
-                        <a x-on:click="$wire.set('showModal', false)" class="btn btn-primary">
-                            {{ __('Cancel') }}
-                        </a>
-
-                        <button type="submit" class="btn btn-error ms-3">
-                            {{ __('Delete Account') }}
-                        </button>
-
-                    </div>
-                </form>
+        <x-modal wire:model="showModal">
+            <div class="flex items-center gap-4 p-6">
+                <svg class="w-8 h-8 text-primary" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18">
+                    </path>
+                </svg>
+                <h2 class="text-lg font-medium">
+                    {{ __('Are you sure you want to delete your account?') }}
+                </h2>
             </div>
-        </dialog>
+
+            <form wire:submit.prevent="deleteUser" class="pb-6">
+                <p class="px-6 mt-1 text-sm">
+                    {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                </p>
+
+                <label class="px-6 mt-6 form-control w-full">
+                    <div class="label">
+                        <span class="label-text">Password</span>
+                    </div>
+
+                    <input wire:model="password"
+                           id="password"
+                           name="password"
+                           type="password"
+                           class="input-bordered w-full max-w-xs input"
+                           placeholder="{{ __('Password') }}" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </label>
+
+                <div class="divider"></div>
+
+                <div class="px-6 flex justify-end">
+                    <button x-on:click.prevent="$wire.set('showModal', false)" class="btn btn-primary">
+                        {{ __('Cancel') }}
+                    </button>
+
+                    <button type="submit" class="btn btn-error ms-3">
+                        {{ __('Delete Account') }}
+                    </button>
+                </div>
+            </form>
+        </x-modal>
     @endteleport
 </div>
--- laravel-livewire/resources/views/livewire/tasks/create-task-modal.blade.php
+++ laravel-livewire/resources/views/livewire/tasks/create-task-modal.blade.php
@@ -2,68 +2,61 @@
     @can('create', \App\Models\Task::class)
         <button x-on:click="$wire.set('showModal', true)" class="btn btn-primary">Add Task</button>
 
-        <dialog class="backdrop-blur-sm backdrop-grayscale modal"
-                x-bind:open="$wire.showModal"
-                x-on:close.stop="$wire.set('showModal', false)"
-                x-on:keydown.escape.window="$wire.set('showModal', false)"
-                x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
-                x-on:keydown.shift.tab.prevent="prevFocusable().focus()">
-            <div class="p-0 modal-box">
-                <div class="flex items-center gap-4 p-6">
-                    <svg class="w-8 h-8 text-primary" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
-                         viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18">
-                        </path>
-                    </svg>
-                    <h3 class="font-bold text-lg">Creating new Task</h3>
-                </div>
-                <form wire:submit="createTask" class="flex flex-col gap-4 px-6">
-                    <label class="form-control w-full">
+        <x-modal wire:model="showModal">
+            <div class="flex items-center gap-4 p-6">
+                <svg class="w-8 h-8 text-primary" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                     viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18">
+                    </path>
+                </svg>
+                <h3 class="font-bold text-lg">Creating new Task</h3>
+            </div>
+            <form wire:submit="createTask" class="flex flex-col gap-4 px-6">
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text">What is the task?</span>
+                    </div>
+                    <input wire:model="form.message" type="text" placeholder="Eg. Do the thing..."
+                           class="input-bordered w-full input" />
+                    <x-input-error :messages="$errors->get('form.message')" class="mt-2" />
+                </label>
+
+                <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
+                    <label class="form-control">
                         <div class="label">
-                            <span class="label-text">What is the task?</span>
+                            <span class="label-text">Category</span>
                         </div>
-                        <input wire:model="form.message" type="text" placeholder="Eg. Do the thing..."
-                               class="input-bordered w-full input" />
-                        <x-input-error :messages="$errors->get('form.message')" class="mt-2" />
+                        <select wire:model="form.category" class="select-bordered select">
+                            <option :value="null">Pick one</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                        <x-input-error :messages="$errors->get('form.category')" class="mt-2" />
                     </label>
 
-                    <div class="gap-4 grid grid-cols-1 md:grid-cols-2">
-                        <label class="form-control">
-                            <div class="label">
-                                <span class="label-text">Category</span>
-                            </div>
-                            <select wire:model="form.category" class="select-bordered select">
-                                <option :value="null">Pick one</option>
-                                @foreach ($categories as $category)
-                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                @endforeach
-                            </select>
-                            <x-input-error :messages="$errors->get('form.category')" class="mt-2" />
-                        </label>
+                    <label class="form-control">
+                        <div class="label">
+                            <span class="label-text">Priority</span>
+                        </div>
+                        <select wire:model="form.priority" class="select-bordered select">
+                            <option :value="null">Pick one</option>
+                            @foreach ($priorities as $priority)
+                                <option value="{{ $priority->id }}">{{ $priority->name }}</option>
+                            @endforeach
+                        </select>
+                        <x-input-error :messages="$errors->get('form.priority')" class="mt-2" />
+                    </label>
+                </div>
+            </form>
 
-                        <label class="form-control">
-                            <div class="label">
-                                <span class="label-text">Priority</span>
-                            </div>
-                            <select wire:model="form.priority" class="select-bordered select">
-                                <option :value="null">Pick one</option>
-                                @foreach ($priorities as $priority)
-                                    <option value="{{ $priority->id }}">{{ $priority->name }}</option>
-                                @endforeach
-                            </select>
-                            <x-input-error :messages="$errors->get('form.priority')" class="mt-2" />
-                        </label>
-                    </div>
-                </form>
-
-                <div method="dialog" class="modal-backdrop">
-                    <div class="gap-4 grid grid-cols-2 mt-4 px-6 py-4 border-t border-base-300">
-                        <button wire:click="createTask" class="text-primary-content btn btn-primary">Create Task</button>
-                        <button x-on:click="$wire.set('showModal', false)" class="bg-base-200 border border-base-300 btn">Close</button>
-                    </div>
+            <div method="dialog" class="modal-backdrop">
+                <div class="gap-4 grid grid-cols-2 mt-4 px-6 py-4 border-t border-base-300">
+                    <button wire:click="createTask" class="text-primary-content btn btn-primary">Create Task</button>
+                    <button x-on:click="$wire.set('showModal', false)" class="bg-base-200 border border-base-300 btn">Close</button>
                 </div>
             </div>
-        </dialog>
+        </x-modal>
     @endcan
 </div>
