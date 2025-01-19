c4b0349823c92e4201657baaaa282440318b664f
Alexis Tan <alexis@sen-dev.com>
2025-01-19 20:18:13 +0100

    Install Nuxt with Vue 3 using Node 22

---
+++ nodejs-nuxt-vue/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// TODO: Check if the current user is authenticated, if they
+// are we can redirect them to the dashboard, if not we can
+// redirect them to the login page
+
+await navigateTo({
+    path: '/dashboard'
+})
+</script>
