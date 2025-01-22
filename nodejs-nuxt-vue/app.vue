6f18c426655183005ff04c37c7fd3923a6552ab6
Alexis Tan <alexis@sen-dev.com>
2025-01-22 13:44:01 +0100

    Fix theme selector working between page changes

--- nodejs-nuxt-vue/app.vue
+++ nodejs-nuxt-vue/app.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const route = useRoute()
+
+watch(route, () => {
+    if (typeof themeChange === 'function') {
+        themeChange(false)
+    }
+})
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
