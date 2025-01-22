dec9024ef4bd33bc8c4d1f7e814d9474badde7d0
Alexis Tan <alexis@sen-dev.com>
2025-01-23 00:29:01 +0100

    Setup creating default records when creating new users

--- nodejs-nuxt-vue/server/api/auth/register.post.ts
+++ nodejs-nuxt-vue/server/api/auth/register.post.ts
@@ -31,6 +31,8 @@ export default defineEventHandler(async (event) => {
         },
     })
 
+    createDefaultUserRecords(user).catch(console.error)
+
     await replaceUserSession(event, {
         user: {
             id: user.id,
