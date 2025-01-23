339bed03a714d118b63b94f35a8a0b1e9033901c
Alexis Tan <alexis@sen-dev.com>
2025-01-23 17:23:47 +0100

    Add support for modals with headless ui

--- nodejs-nuxt-vue/pages/profile.vue
+++ nodejs-nuxt-vue/pages/profile.vue
@@ -8,6 +8,8 @@ $fetch('/api/user').then((response) => {
     userForm.email = response.data.email
 })
 
+const deleteAccountModal = ref(false)
+
 const errors = ref(null)
 const userForm = reactive({
     name: '',
@@ -117,7 +119,7 @@ const updatePassword = () => {
                             </div>
                             <input v-model="passwordForm.current_password" type="password"
                                 class="input-bordered w-full max-w-xs input" />
-                                <InputError v-model="errors" name="current_password" />
+                            <InputError v-model="errors" name="current_password" />
                         </label>
                         <label class="form-control px-4 w-full">
                             <div class="label">
@@ -125,7 +127,7 @@ const updatePassword = () => {
                             </div>
                             <input v-model="passwordForm.password" type="password"
                                 class="input-bordered w-full max-w-xs input" />
-                                <InputError v-model="errors" name="password" />
+                            <InputError v-model="errors" name="password" />
                         </label>
                         <label class="form-control px-4 w-full">
                             <div class="label">
@@ -133,7 +135,7 @@ const updatePassword = () => {
                             </div>
                             <input v-model="passwordForm.password_confirmation" type="password"
                                 class="input-bordered w-full max-w-xs input" />
-                                <InputError v-model="errors" name="password_confirmation" />
+                            <InputError v-model="errors" name="password_confirmation" />
                         </label>
                     </div>
 
@@ -165,8 +167,51 @@ const updatePassword = () => {
                     retain.
                 </p>
 
-                <button class="text-error-content btn btn-error">Delete Account</button>
+                <button class="text-error-content btn btn-error" @click="deleteAccountModal = true">Delete Account</button>
             </div>
         </div>
     </div>
+
+    <Modal :open="deleteAccountModal" @close="deleteAccountModal = false">
+        <div class="flex items-center gap-4 p-6">
+            <svg class="w-8 h-8 text-error" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor"
+                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z">
+                </path>
+            </svg>
+
+            <h2 class="font-medium text-lg">
+                Are you sure you want to delete your account?
+            </h2>
+        </div>
+
+        <form class="pb-6" method="dialog">
+            <p class="mt-1 px-6 text-sm">
+                Once your account is deleted, all of its resources and data will be permanently deleted.
+                Please enter your password to confirm you would like to permanently delete your account.
+            </p>
+
+            <label class="form-control mt-6 px-6 w-full">
+                <div class="label">
+                    <span class="label-text">Password</span>
+                </div>
+
+                <input id="password" name="password" type="password" class="input-bordered w-full max-w-xs input"
+                    placeholder="Password" />
+            </label>
+
+            <div class="divider"></div>
+
+            <div class="flex justify-end px-6">
+                <a @click="deleteAccountModal = false" class="btn btn-primary">
+                    Cancel
+                </a>
+
+                <button type="submit" class="btn btn-error ms-3">
+                    Delete Account
+                </button>
+            </div>
+        </form>
+    </Modal>
 </template>
