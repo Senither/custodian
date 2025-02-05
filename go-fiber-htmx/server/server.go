1f4f4788bf69f8fd29aba420423d604488d78dfc
Alexis Tan <alexis@sen-dev.com>
2025-02-05 11:25:16 +0100

    Add 404 error page handler

--- go-fiber-htmx/server/server.go
+++ go-fiber-htmx/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/template/jet/v2"
+	"github.com/senither/custodian/config"
 	"github.com/senither/custodian/server/router"
 )
 
@@ -15,14 +16,23 @@ type ServerConfig struct {
 	ViewFilesystem   embed.FS
 }
 
-func NewServer(config ServerConfig) *fiber.App {
-	app := createNewFiberApp(config)
+func NewServer(cfg ServerConfig) *fiber.App {
+	app := createNewFiberApp(cfg)
 
 	router.RegisterRoutes(app)
 
 	app.Use("", filesystem.New(filesystem.Config{
-		Root: http.FS(config.PublicFilesystem),
+		Root: http.FS(cfg.PublicFilesystem),
 	}))
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.
+			Status(fiber.StatusNotFound).
+			Render("views/errors/404", fiber.Map{
+				"ApplicationName":       config.GetString("APP_NAME"),
+				"ApplicationDescriptor": config.GetString("APP_DESCRIPTOR"),
+			}, "views/layouts/guest")
+	})
 
 	return app
 }
